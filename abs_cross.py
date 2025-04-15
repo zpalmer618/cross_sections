db5cdac367bc14eca21d8328e03751db00fad259
Zach Palmer <zpalmer618@gmail.com>
2025-04-15 14:46:18 -0500

    formatted with ruff

--- abs_cross.py
+++ abs_cross.py
@@ -3,44 +3,50 @@ import sys
 
 # JWST resolution ranges
 res_range = [
-(1.66, 3.17, 2700),
-(4.885, 5.751, 3630),
-(5.634, 6.632, 3620),
-(6.408, 7.524, 3590),
-(7.477, 8.765, 3520),
-(8.711, 10.228, 3290),
-(10.017, 11.753, 3250),
-(11.481, 13.441, 3010),
-(13.319, 15.592, 2370),
-(15.4, 18.072, 2470),
-(17.651, 20.938, 1460),
-(20.417, 24.22, 1680),
-(23.884, 28.329, 1630),
+    (1.66, 3.17, 2700),
+    (4.885, 5.751, 3630),
+    (5.634, 6.632, 3620),
+    (6.408, 7.524, 3590),
+    (7.477, 8.765, 3520),
+    (8.711, 10.228, 3290),
+    (10.017, 11.753, 3250),
+    (11.481, 13.441, 3010),
+    (13.319, 15.592, 2370),
+    (15.4, 18.072, 2470),
+    (17.651, 20.938, 1460),
+    (20.417, 24.22, 1680),
+    (23.884, 28.329, 1630),
 ]
 
 # This function loads the frequencies and intensities from a file
 # and returns them as two separate lists.
 # The input file should contain a frequency and an intensity separated by whitespace.
 
+
 def load_freqs(filename):
     list_freq = []
     list_int = []
-    with open(filename, 'r') as infile:
+    with open(filename, "r") as infile:
         for lineno, line in enumerate(infile, start=1):
             parts = line.strip().split()
             if len(parts) != 2:
-                print(f"Error: Input file '{filename}' is not formatted correctly on line {lineno}.")
-                print("Each line should contain a frequency and an intensity separated by whitespace.") 
+                print(
+                    f"Error: Input file '{filename}' is not formatted correctly on line {lineno}."
+                )
+                print(
+                    "Each line should contain a frequency and an intensity separated by whitespace."
+                )
                 exit(1)
             # The following if statement is not necessary given the above 'if not' statement.
             # It is left here to remind me later.
-            #if len(parts) == 2:
+            # if len(parts) == 2:
             freq, intensity = map(float, parts)
             list_freq.append(freq)
             list_int.append(intensity)
         return list_freq, list_int
 
-# TO-DO: Check if the frequencies have associated resolutions. 
+
+# TO-DO: Check if the frequencies have associated resolutions.
 # If not, print a warning message on the line where the resolution is missing.
 # This will likely require 'major' refactoring to thake the list_micron for loop
 # and convert it into a function that accepts frequencies as an argument.
@@ -49,6 +55,7 @@ def get_R(wavelength, ranges):
         if low <= wavelength <= high:
             return float(res)
     return None
+
 
 wave_to_mu = 10000.0
 
@@ -69,7 +76,9 @@ for micron in list_micron:
         del_lambda = wave_to_mu * (R / micron)
         list_del_lambda.append(del_lambda)
     else:
-        print(f"Warning: No resolution found for {10000 / micron} cm\u207b\u00b9. If this is a mistake, double check the input file and try again.")
+        print(
+            f"Warning: No resolution found for {10000 / micron} cm\u207b\u00b9. If this is a mistake, double check the input file and try again."
+        )
 
 list_epmax = []
 for del_lambda, intensity in zip(list_del_lambda, list_int):
@@ -78,9 +87,9 @@ for del_lambda, intensity in zip(list_del_lambda, list_int):
 
 list_acs = []
 for epmax in list_epmax:
-    acs = (math.log(10) * (1000 / 6.022E23)) * epmax
+    acs = (math.log(10) * (1000 / 6.022e23)) * epmax
     list_acs.append(acs)
 
-print('Frequency (cm\u207b\u00b9)', '=', 'Absorption Cross Section (cm\u00b2/molecule)')
+print("Frequency (cm\u207b\u00b9)", "=", "Absorption Cross Section (cm\u00b2/molecule)")
 for i, j in zip(list_freq, list_acs):
-    print(f'{i} = {j:.1e}')
+    print(f"{i} = {j:.1e}")
